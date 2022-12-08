dbb86d53d2ec4eea1c81fc52b07eefd236cb402d
milov52 <111310007+milov52@users.noreply.github.com>
2022-12-08 15:52:24 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,9 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# DEBUG = env.bool('DEBUG', default=False)
-DEBUG = False
-
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', '[::1]'])
 
